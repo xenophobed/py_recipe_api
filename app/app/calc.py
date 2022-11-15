5a126e17cd515e954ff1952e423453ed8bcb8eb8
dennis <xenodennis@denniss-MacBook-Air.local>
2022-11-15 13:17:47 +0800

    psycopg2 included in the image

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+calculator functions
+"""
+
+
+def add(x,y):
+    """Add x and y and return result"""
+    return x+y
+
+def substract(x,y):
+    """substract x and y and return result"""
+    return y-x
---
+++ app/app/tests.py
@@ -0,0 +1,24 @@
+"""
+sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class calcTest(SimpleTestCase):
+    """Test the calc module"""
+
+    def test_add_numbers(self):
+        """Test adding numbers together."""
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
+
+
+    def test_substract(self):
+        """Test substracting numbers"""
+        res = calc.substract(10, 15)
+
+        self.assertEqual(res, 5)
